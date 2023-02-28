2c1dfc22f2acca3752a3523371bc6d2789edf3f1
Stanislav Valkov <stanislavyv31@gmail.com>
2023-02-28 15:57:07 +0200

    Add category component

---
+++ cartridges/plugin_page_designer_components/cartridge/experience/components/commerce_assets/category.js
@@ -0,0 +1,34 @@
+"use strict";
+const Template = require("dw/util/Template");
+const HashMap = require("dw/util/HashMap");
+const URLUtils = require("dw/web/URLUtils");
+const ImageTransformation = require("*/cartridge/experience/utilities/ImageTransformation.js");
+
+/**
+ * Render logic for the Hero Content asset
+ * @param {dw.experience.ComponentScriptContext} context The Component script context object.
+ *
+ * @returns {string} The markup to be displayed
+ */
+module.exports.render = function (context) {
+    const model = new HashMap();
+    const content = context.content;
+
+    model.categoryLink = URLUtils.url(
+        "Search-Show",
+        "cgid",
+        content.categoryId
+    ).toString();
+    model.categoryDescription = content.categoryDescription;
+
+    model.image = ImageTransformation.getScaledImage(content.image);
+    model.ctaTitle = content.ctaTitle;
+    model.isCtaPositionedAtTop = content.ctaPosition === "top";
+
+    const cacheForOneDay = require("~/cartridge/scripts/helpers/cacheForOneDay.js");
+    cacheForOneDay(response);
+
+    return new Template(
+        "experience/components/commerce_assets/category"
+    ).render(model).text;
+};
